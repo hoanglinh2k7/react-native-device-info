b957fdeabdd8b73015e4231300fcf3efd369bd43
hoanglinh2k7 <33474102+hoanglinh2k7@users.noreply.github.com>
2017-12-08 16:22:49 +0700

    Update RNDeviceInfo.java

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceInfo.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceInfo.java
@@ -22,11 +22,6 @@ public class RNDeviceInfo implements ReactPackage {
     return modules;
   }
 
-  // Deprecated RN 0.47
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-  	return Collections.emptyList();
-  }
-
   @Override
   public List<ViewManager> createViewManagers(
                             ReactApplicationContext reactContext) {
